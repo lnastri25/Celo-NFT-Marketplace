29efd35e57dff70d19b711e78f815954fccee869
Lorenzo <lorenzonastri@gmail.com>
2023-01-10 12:59:26 -0300

    Celo NFT Marketplace

---
+++ frontend/constants.js
@@ -0,0 +1,3 @@
+export const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/lnastri25/celo-nft-marketplace";
+export const MARKETPLACE_ADDRESS = "0xac4F143f7Fc1844403948541dA5106eb80F30E22";
